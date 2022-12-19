3ef8b8eecfd5e25e4f796e252779c40c269ae3a5
CST81 <cyril.treichler@gmail.com>
2022-12-19 22:50:35 +0100

    Adding 4 songs to an array and printing them separately.

---
+++ array_songs.py
@@ -0,0 +1,6 @@
+favorite_songs = ["highway to hell", "jumping jack flash", "running through the jungle", "happy"]
+
+print(favorite_songs[0])
+print(favorite_songs[1])
+print(favorite_songs[2])
+print(favorite_songs[3])
